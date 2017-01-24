4ceb6af67161f09ac9865778e3f13be14c1c1b3c
Meghan Debity <mdebity@gmail.com>
2017-01-24 17:54:35 -0600

    i have no idea what i'm doing

--- sonnet.js
+++ sonnet.js
@@ -1,19 +1,30 @@
 
 // Take the contents of the sonnet div and place it in a variable
-var sonnet = document.getElementById("sonnet");
-//above line grabs from the div
-console.log("sonnet", sonnet);
-var sonnet = element.innerHTML;
-
-// var element = document.getElementById("sonnet");
-// console.log(element);
+// var sonnet = document.getElementById("sonnet");
+// //above line grabs from the div
+// console.log("sonnet", sonnet);
 // var sonnet = element.innerHTML;
 
+var element = document.getElementById("sonnet");
+console.log(element);
+var sonnet = element.innerHTML;
 
 // Find and output the starting position of the word "orphans"
-("sonnet").find('orphans');
+//("sonnet").find('orphans');
 
+var indexOfOrphans = sonnet.indexOf("orphans");
+console.log(indexOfOrphans);
 
+alert("There are " + sonnet.length +" charcters in this sonnet");
+
+var textreplace = sonnet.replace("winter", "yuletide");
+console.log(textreplace);
+
+var globalreplace = sonnet.replace(/the/g, "a large");
+console.log(globalreplace);
+
+document.getElementById("sonnet").innerHTML = "The quick brown fox jumped over the lazy dog!"
+console.log(sonnet);
 
 
 // Output the number of characters in the sonnet
