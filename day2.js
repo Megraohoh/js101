a688aaa403891344dd118c74c977dd09b9ebcec2
Meghan Debity <mdebity@gmail.com>
2017-01-24 19:21:48 -0600

    switch review

---
+++ day2.js
@@ -0,0 +1,25 @@
+//switch statement
+
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+		console.log("Booooo");
+	case "Friday":
+		console.log("PARTAY");
+		break;
+	default:
+		console.log("not monday");
+}
+
+
+// the above does the same as:
+
+
+// if(day === "Monday"){
+// 	console.log("Booooo");
+// } else if(day === "Friday"){
+// 	console.log("PARTAY");
+// } else{
+// 	console.log("not monday");
+// }
